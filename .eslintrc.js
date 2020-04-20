cb1d9cd7bd755321cb2256e095bee395f4f5ab06
Geoffrey Hanson <g.hanson.b@gmail.com>
2020-04-20 13:47:12 -0500

    Finished initial setup, started basic functionality

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+    env: {
+      browser: true,
+      es6: true,
+    },
+    extends: [
+      'plugin:react/recommended',
+      'airbnb',
+    ],
+    parser: 'babel-eslint',
+    globals: {
+      Atomics: 'readonly',
+      SharedArrayBuffer: 'readonly',
+    },
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+      ecmaVersion: 2018,
+      sourceType: 'module',
+    },
+    plugins: [
+      'react',
+    ],
+    rules: {
+      'react/jsx-filename-extension': 0,
+      'indent': ['error', 2],
+      'global-require': 1,
+      'react/require-default-props': 0,
+      'react/prefer-stateless-function': 0,
+    },
+  };
